df9c667ac2bd5d9046ba54bbb4d86b3b0b6599e8
goudete <goudetenrique@gmail.com>
2023-05-08 16:55:56 -0400

    gtfol

---
+++ pages/api/generate.ts
@@ -0,0 +1,43 @@
+const generateAction = async (req, res) => {
+  console.log('Received request');
+
+  const input = JSON.parse(req.body).input;
+
+  const response = await fetch(
+    `https://api-inference.huggingface.co/models/goudete/experiment`,
+    {
+      headers: {
+        Authorization: `Bearer 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`,
+        'Content-Type': 'application/json',
+        'x-use-cache': 'false'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        inputs: input,
+      }),
+    }
+  );
+
+  if (response.ok) {
+    const buffer = await response.arrayBuffer();
+    const base64 = bufferToBase64(buffer);
+    res.status(200).json({ image: base64 });
+  } else if (response.status === 503) {
+    const json = await response.json();
+    res.status(503).json(json);
+  } else {
+    const json = await response.json();
+    console.log('ERROR: ', json)
+    res.status(response.status).json({ error: response.statusText });
+  }
+};
+
+const bufferToBase64 = (buffer) => {
+  let arr = new Uint8Array(buffer);
+  const base64 = btoa(
+    arr.reduce((data, byte) => data + String.fromCharCode(byte), '')
+  )
+  return `data:image/png;base64,${base64}`;
+};
+
+export default generateAction;
